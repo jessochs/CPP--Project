98db415ae2b6577013d7ac2607ca463e7b8d40c8
jessochs <fac17002@byui.edu>
2024-03-23 09:56:54 -0700

    Add project files.

---
+++ Hangman/Hangman.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include "Hangman.h"
+
+using namespace std;
+
+void incorrect_guesses(vector<char> wrong, string answer)
+{
+	cout << "\nIncorrect Letters Guessed: " << endl;
+
+	for (int i = 0; i<wrong.size(); i++)
+	{
+		cout << wrong[i] << endl;
+	}
+
+	cout << "\nSecret Word:" << endl;
+
+	for (int i = 0; i < answer.length(); i++)
+	{
+		cout << answer[i];
+	}
+}
+
+void end(string answer, string secret_word)
+{
+	if (answer == secret_word)
+	{
+		cout << "Congrats! You won!" << endl;
+	}
+	else
+	{
+		cout << "You failed at guessing the word!" << endl;
+	}
+}
---
+++ Hangman/Home.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include "Hangman.h"
+
+
+using namespace std;
+
+int main()
+{
+	cout << "****** Hangman Game ******" << endl;
+	cout << "**************************" << endl;
+	cout << "Try to guess the word before your tries run out!" << endl;
+
+	string secret_word = "something";
+
+	string answer = "_________";
+	int misses = 0;
+	vector<char> wrong;
+	bool guess = false;
+	char letter;
+
+	while (answer != secret_word && misses < 5)
+	{
+		incorrect_guesses(wrong, answer);
+
+		cout << "\nPlease enter a letter";
+		cin >> letter;
+
+		for (int i = 0; i < secret_word.length(); i++)
+		{
+			if (letter == secret_word[i])
+			{
+				answer[i] = letter;
+				guess = true;
+			}
+		}
+		if (guess)
+		{
+			cout << "\nYou've guessed correctly!" << endl;
+
+		}
+		else
+		{
+			cout << "\nIncorrect. Once life has been lost" << endl;
+			wrong.push_back(letter);
+			misses++;
+		}
+		guess = false;
+
+	}
+
+	end(answer, secret_word);
+
+	return 0;
+}
